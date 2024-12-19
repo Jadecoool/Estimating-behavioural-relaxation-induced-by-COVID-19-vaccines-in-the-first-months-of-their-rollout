bf6ee4f5d131a4ac6c73a975001ebc51bc47f99e
yuhanli <78245294+Jadecoool@users.noreply.github.com>
2024-12-19 16:10:12 +0000

    Update variation_model_two_strains2_data2.py

--- code/calibration/variation_model_two_strains2_data2.py
+++ code/calibration/variation_model_two_strains2_data2.py
@@ -9,7 +9,6 @@ from functions2wave_data2 import import_country, get_totR, update_contacts, get_
 import math
 ncomp = 15
 nage = 16
-np.random.seed(0)
 
 def SEIR_variation(start_date_org,
                   end_date,
@@ -89,7 +88,7 @@ def SEIR_variation(start_date_org,
     # number of age groups
     n_age = len(Nk)
 
-    country_dict = import_country(basin, path_to_data='../data2')
+    country_dict = import_country(basin, path_to_data='../data')
 
     deltadays = end_date - start_date
     dates = [start_date + timedelta(days=d) for d in range(deltadays.days)]
